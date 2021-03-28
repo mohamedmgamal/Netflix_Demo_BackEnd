f3797462a3cc404101f55efef6ff8bdf454b650b
mohamedmgamal <mohamedmgamal_97@yahoo.com>
2021-03-28 17:23:09 +0200

    remove changes

--- Netflix_Demo_BackEnd/settings.py
+++ Netflix_Demo_BackEnd/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x*ybgk^36x0$k-i9@qxpp=jj5$*+0&ntfdyt93yxg_s%42_&om'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["netflix-demo-iti.herokuapp.com","127.0.0.1"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -78,32 +78,32 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Netflix_Demo_BackEnd.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME':'Netflix',
-#         'USER': 'postgres',
-#         'PASSWORD': 'admin',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
-
+#Database ={}
+#https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'dcj5pi64m744bl',
-        'USER': 'tqyuqmdfvunery',
-        'PASSWORD': 'e9b9b53af8933d9063deb9542e3e7f14983b8bbd96c9a622b5003cd0690fd4d1',
-        'HOST': 'ec2-54-155-208-5.eu-west-1.compute.amazonaws.com',
+        'NAME':'Netflix',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
         'PORT': '5432',
-        'URI': 'postgres://tqyuqmdfvunery:e9b9b53af8933d9063deb9542e3e7f14983b8bbd96c9a622b5003cd0690fd4d1@ec2-54-155-208-5.eu-west-1.compute.amazonaws.com:5432/dcj5pi64m744bl',
-        'Heroku CLI': 'heroku pg:psql postgresql-flexible-90856 --app netflix-demo-iti'
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':'dcj5pi64m744bl',
+#         'USER': 'tqyuqmdfvunery',
+#         'PASSWORD': 'e9b9b53af8933d9063deb9542e3e7f14983b8bbd96c9a622b5003cd0690fd4d1',
+#         'HOST': 'ec2-54-155-208-5.eu-west-1.compute.amazonaws.com',
+#         'PORT': '5432',
+#         'URI': 'postgres://tqyuqmdfvunery:e9b9b53af8933d9063deb9542e3e7f14983b8bbd96c9a622b5003cd0690fd4d1@ec2-54-155-208-5.eu-west-1.compute.amazonaws.com:5432/dcj5pi64m744bl',
+#         'Heroku CLI': 'heroku pg:psql postgresql-flexible-90856 --app netflix-demo-iti'
+#     }
+# }
 
 
 # Password validation

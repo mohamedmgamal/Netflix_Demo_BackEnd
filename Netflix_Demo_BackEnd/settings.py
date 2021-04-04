c86428c2c32fa18f9c7e0881309ddc275892bea2
Mohamed Gamal <mohamedmgamal_97@yahoo.com>
2021-04-04 15:37:24 +0200

    static files added

--- Netflix_Demo_BackEnd/settings.py
+++ Netflix_Demo_BackEnd/settings.py
@@ -141,5 +141,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 AUTH_USER_MODEL = "RestApi.User"
 STATIC_URL = '/static/'
